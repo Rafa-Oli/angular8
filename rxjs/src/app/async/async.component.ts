1940d372fcea90c8e54db328848b8725d1ace139
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-06-16 19:54:57 -0300

    async

--- rxjs/src/app/async/async.component.ts
+++ rxjs/src/app/async/async.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { delay, map, toArray } from 'rxjs/operators';
 
+interface User{
+  login: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-async',
   templateUrl: './async.component.html',
@@ -10,6 +15,7 @@ import { delay, map, toArray } from 'rxjs/operators';
 export class AsyncComponent implements OnInit {
 
   private options$: Observable<string[]>
+  private user$: Observable<User>
 
   constructor() { }
 
@@ -26,7 +32,22 @@ export class AsyncComponent implements OnInit {
         toArray(),
         delay(2000)
       )
-      this.options$.subscribe((s) => console.log(s))
+     // this.options$.subscribe((s) => console.log(s))
+
+
+     this.user$ = new Observable<User>((observer) =>{
+       let names = ['Mr.James', 'Mr.John', 'Mr.Ray', 'Ms.Angel'];
+       let logins = ['James', 'John', 'Ray', 'Angel'];
+       let i = 0;
+       setInterval(() => {
+         if(i==4)
+         observer.complete()
+         else observer.next({login: logins[i], name: names[i]});
+         i++
+       }, 3000)
+     })
+
+    this.user$.subscribe((s) => console.log(s))
   }
 
 }
