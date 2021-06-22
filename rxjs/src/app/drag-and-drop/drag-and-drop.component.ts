40835ac97452476cb7ca99390667e79c69285fa7
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-06-21 21:39:18 -0300

    drag and drop

--- rxjs/src/app/drag-and-drop/drag-and-drop.component.ts
+++ rxjs/src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-drag-and-drop',
@@ -15,6 +17,30 @@ export class DragAndDropComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    let mousedown = fromEvent(this.myrect.nativeElement, 'mousedown');
+    let mousemove = fromEvent(document, 'mousemove');
+    let mouseup = fromEvent(document, 'mouseup');
+
+    mousedown.subscribe((ed: MouseEvent) => {
+      let x = ed.pageX;
+      let y = ed.pageY;
+
+      mousemove
+      .pipe(takeUntil(mouseup))
+      .subscribe((em: MouseEvent) => {
+        let offsetx = x - em.pageX;
+        let offsety = y - em.pageY;
+        this.top -= offsety;
+        this.left -= offsetx; 
+
+        x = em.pageX;
+        y = em.pageY;
+
+      })
+    })
   }
 
 }
