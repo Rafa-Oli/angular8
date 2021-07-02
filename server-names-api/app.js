759b41eb26504c7f2514abab95077fd0494e34dc
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-07-01 21:24:12 -0300

    test api

--- server-names-api/app.js
+++ server-names-api/app.js
@@ -9,7 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/namesdb", {
+  useNewUrlParser: true,
+});
 
 app.get("/", (req, res) => {
   Person.find({})
@@ -23,6 +25,30 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/:text", (req, res) => {
+  let text = req.params.text;
+  var query = {
+    $or: [
+      { firstname: { $regex: text, $options: "i" } },
+      { lastname: { $regex: text, $options: "i" } },
+      { country: { $regex: text, $options: "i" } },
+      { email: { $regex: text, $options: "i" } },
+      { city: { $regex: text, $options: "i" } },
+    ],
+  };
+  Person.find(query)
+    .lean()
+    .exec((err, data) => {
+      if (err) {
+        return res.status(500).json({ error: err, message: err.message });
+      }
+
+      return res.status(200).json(data);
+    });
+});
+
 app.use(function (req, res, next) {
   res.status(404).send("Route does not exist");
 });
+
+app.listen(9000);
